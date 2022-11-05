e5f39963f82b60beb39265170f70309e2f283547
Tam <tam128974563@gmail.com>
2022-11-05 09:43:15 +0700

    switchLang Front PAge

--- src/route.js
+++ src/route.js
@@ -2,28 +2,63 @@ const {
     Router,
 } = require('express');
 
-const pages = {
-    "vi": ["trang-chu", "ve-chung-toi", "dich-vu", "ung-dung", "du-an"],
-    "en": ["index", "about", "services", "contact", "projects", "application"]
-};
+// const pages = {
+//     "vi": ["trang-chu", "ve-chung-toi", "dich-vu", "ung-dung", "du-an"],
+//     "en": ["index", "about", "services", "contact", "projects", "application"]
+// };
+const pages = [{
+    en: " ",
+    vi: "trang-chu"
+}, {
+    en: "about",
+    vi: "ve-chung-toi"
+}, {
+    en: "application",
+    vi: "ung-dung"
+}, {
+    en: "services",
+    vi: "dich-vu"
+}, {
+    en: "projects",
+    vi: "du-an"
+}, {
+    en: "contact",
+    vi: "lien-he"
+}, {
+    en: "",
+    vi: ""
+}, {
+    en: "",
+    vi: ""
+}, {
+    en: "",
+    vi: ""
+}]
 const homePage = (req, res) => {
     res.render('index', {
         lang: "en",
-        page: "index",
+        page: " ",
+        en: " ",
+        vi: "trang-chu",
     });
 }
 const enPageController = (req, res) => {
+    const url = pages.filter(item => item.en === `${req.params.id}`)[0];
     res.render(`${req.params.id}`, {
         lang: "en",
         page: req.params.id,
+        en: url.en,
+        vi: url.vi,
 
     });
 }
 const viPageController = (req, res) => {
+    const url = pages.filter(item => item.vi === `${req.params.id}`)[0];
     res.render(`${req.params.id}`, {
         lang: "vi",
         page: req.params.id,
-
+        en: url.en,
+        vi: url.vi,
     });
 }
 
