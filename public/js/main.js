8cb04bdaa21f9334516657bc8e40108b0e5e29d2
daile107 <daile107107@gmail.com>
2023-03-15 14:00:17 +0700

    update project

--- public/js/main.js
+++ public/js/main.js
@@ -288,7 +288,8 @@
         $(".filtering").on("click", "span", function () {
             var a = $(".gallery").isotope({});
             var e = $(this).attr("data-filter");
-            a.isotope({ filter: e });
+            a.isotope({ filter: e,
+                layoutMode: 'fitRows' });
         });
         $(".filtering").on("click", "span", function () {
             $(this).addClass("active").siblings().removeClass("active");
